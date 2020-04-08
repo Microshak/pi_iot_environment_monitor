7d07f40eb64da3053e2b9f12a060129c5c03ed7c
microshak <roshak@hotmail.com>
2020-04-07 17:33:24 -0700

    l

--- telemerty.py
+++ telemerty.py
@@ -8,22 +8,20 @@ CONNECTION_STRING = "HostName=University-IoT-Cart.azure-devices.net;DeviceId=Pi_
 TEMPERATURE = 20.0
 HUMIDITY = 60
 MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
-
 INTERVAL = 1
 
-def iothub_client_init():
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-    return client
+def getReadings():
+    temperature = TEMPERATURE + (random.random() * 15)
+    humidity = HUMIDITY + (random.random() * 20)
+    msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+    message = Message(msg_txt_formatted)
+    return(message)
 
-
-
-
-def iothub_client_telemetry_sample_run():
+def iothub_client_telemetry_run():
 
     try:
-        client = iothub_client_init()
+        client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
-
 
         while True:
             message = getReadings()
@@ -37,13 +35,7 @@ def iothub_client_telemetry_sample_run():
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 
-def getReadings():
-    temperature = TEMPERATURE + (random.random() * 15)
-    humidity = HUMIDITY + (random.random() * 20)
-    msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
-    message = Message(msg_txt_formatted)
-    return(message)
 
 if __name__ == '__main__':
     print ( "Press Ctrl-C to exit" )
-    iothub_client_telemetry_sample_run()
+    iothub_client_telemetry_run()
